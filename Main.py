f0703880bdae8c8760f1ac9ebcc258e8350da7b6
Lucas Militão <lucasmengineer@edu.unifor.br>
2019-09-03 13:08:38 -0300

    Implementados o Perceptro, método para ler arquivos e a plotagem. Pedente a plotagem com os gráfico solicitados junto a reta se possível a separacão

--- Main.py
+++ Main.py
@@ -1,28 +1,103 @@
 from Perceptron import Perceptron
 from Dataset import Dataset
+import matplotlib.pyplot as matPlot  # Para visualizacao dos datasets
+from matplotlib.colors import ListedColormap  # Lista de cores para plotagens
+import pandas as panda  # leitor de arquivo CSV
 
-#valores do exercício para teste
-# entradas = [[0.5, 1.5], [-0.5, 0.5], [0.5, 0.5], [0.5, -0.3], [1.5, 0.5]]
-# saidasDesejadas = [1, -1, 1, -1, 1]
-# pesosSinapticos = [[1, 2]]
-# taxaAprendizagem = 0.1
-# limiar = [-2]
+# Carregar Dataset#1 treino
+dados_treinamento_csv = panda.read_csv('Dataset_Treino_1.csv', header=None)
+dados_treinamento_csv.head()
+
+# Carregar Dataset#1 teste
+dados_teste_csv = panda.read_csv('Dataset_Teste_1.csv', header=None)
+dados_teste_csv.head()
+
+# Valores da linha 0 a 120 nas posições 0 e 1 são os valores de entrada da função em treinamento
+Xtrain = dados_treinamento_csv.iloc[0:120, [0, 1]].values
+# Valores da linha 0 a 120 na posição 2 são os valores esperados em treinamento
+Ytrain = dados_treinamento_csv.iloc[0:120, 2].values
+
+# Valores da linha 0 a 30 nas posições 0 e 1 são os valores de entrada da função em teste
+Xtest = dados_teste_csv.iloc[0:30, [0, 1]].values
+# Valores da linha 0 a 30 na posição 2 são os valores esperados em treinamento
+Ytest = dados_teste_csv.iloc[0:30, 2].values
+
+# Plot dos valores de Treino
+cm_bright = ListedColormap(['#FF0000', '#0000FF', '#00FF22'])
+matPlot.figure(figsize=(5, 5))
+matPlot.scatter(Xtrain[:, 0], Xtrain[:, 1], c=Ytrain, cmap=cm_bright)
+matPlot.scatter(None, None, color='r', label='Classe 1')
+matPlot.scatter(None, None, color='b', label='Classe 2')
+matPlot.scatter(None, None, color='g', label='Classe 3')
+matPlot.legend()
+matPlot.title('Dataset#1 Treino')
+matPlot.show()
+
+# Plot dos valores de Teste
+cm_bright = ListedColormap(['#FF0000', '#0000FF', '#00FF22'])
+matPlot.figure(figsize=(5, 5))
+matPlot.scatter(Xtest[:, 0], Xtest[:, 1], c=Ytest, cmap=cm_bright)
+matPlot.scatter(None, None, color='r', label='Classe 1')
+matPlot.scatter(None, None, color='b', label='Classe 2')
+matPlot.scatter(None, None, color='g', label='Classe 3')
+matPlot.legend()
+matPlot.title('Dataset#1 Teste')
+matPlot.show()
+########################################################################################################################
 
 def main():
+########################################################################################################################
+# Perceptron para dupla 1x2
+    arquivoD1x2 = open("./dtrain1x2.txt", "r")  # Carrega arquivo D 1x2
+    arquivoX1x2 = open("./xtrain1x2.txt", "r")  # Carrega arquivo X 1x2
+    dataset1x2 = Dataset(arquivoD1x2, arquivoX1x2)  # Padroniza o Dataset
+    teste1x2 = Perceptron(dataset1x2.definindoEntradas(),
+                          dataset1x2.definindoValoresDesejados(), 1)  # Cria uma Perceptron pro dataset 1x2
 
-    arquivoD = open("./dtest.txt", "r")
-    arquivoX = open("./xtest.txt", "r")
+    teste1x2.treinamento()
 
-    dataset = Dataset(arquivoD, arquivoX)
+    print('')
+    print('Resultado do treinamento com Perceptron do Dataset#1 Classes 1 x 2')
+    print('w0: {0}  w1: {1}  w2: {2}'.format(teste1x2.getW()[0], teste1x2.getW()[1], teste1x2.getW()[2]))
+    print('Número de épocas: {0}'.format(teste1x2.epoca))
+    print('')
 
-    teste = Perceptron(dataset.definindoEntradas(), dataset.definindoValoresDesejados(), 1)
+########################################################################################################################
+# Perceptron para dupla 2x3
+    arquivoD2x3 = open("./dtrain2x3.txt", "r")  # Carrega arquivo D 2x3
+    arquivoX2x3 = open("./xtrain2x3.txt", "r")  # Carrega arquivo X 2x3
+    dataset2x3 = Dataset(arquivoD2x3, arquivoX2x3)  # Padroniza o Dataset
+    teste2x3 = Perceptron(dataset2x3.definindoEntradas(),
+                          dataset2x3.definindoValoresDesejados(),
+                          1)  # Cria uma Perceptron pro dataset 2x3
 
-    teste.treinamento()
+    teste2x3.treinamento()
 
-    arquivoD.close()
+    print('Resultado do treinamento com Perceptron do Dataset#1 Classes 2 x 3')
+    print('w0: {0}  w1: {1}  w2: {2}'.format(teste2x3.getW()[0], teste2x3.getW()[1], teste2x3.getW()[2]))
+    print('Número de épocas: {0}'.format(teste2x3.epoca))
+    print('')
 
-    for i in teste.u:
-        print(i)
+    arquivoTesteD2x3 = open("./dtest2x3.txt", "r")  # Carrega arquivo D 2x3
+    arquivoTesteX2x3 = open("./xtest2x3.txt", "r")  # Carrega arquivo X 2x3
+    datasetTeste2x3 = Dataset(arquivoTesteD2x3, arquivoTesteX2x3)
 
-main()
+    teste2x3.predict(datasetTeste2x3.definindoEntradas(),
+                     datasetTeste2x3.definindoValoresDesejados(),
+                     teste2x3.resultado()[0],
+                     teste2x3.resultado()[1])
+
+########################################################################################################################
+# Fechamento dos arquivos de dataset#1
+
+    arquivoD1x2.close()
+    arquivoX1x2.close()
+
+    arquivoD2x3.close()
+    arquivoX2x3.close()
+
+    arquivoTesteX2x3.close()
+    arquivoTesteD2x3.close()
+
+main() #execucão da funcão main
 
--- Perceptron.py
+++ Perceptron.py
@@ -1,78 +1,78 @@
 import random
+from sklearn import preprocessing
 
 class Perceptron:
 
     def __init__(self, entradas, saidasDesejadas, taxaAprendizagem):
+        normalizar = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(entradas)
+        entradas = normalizar.transform(entradas)
+
         self.x = entradas
         self.d = saidasDesejadas
         self.w = []
         self.n = taxaAprendizagem
         self.w0 = []
         self.u = []
+        self.epoca = 0
 
-    #treinamento
-    def treinamento(self): #funcao que ira realizar o treinamento
-
+        # treinamento
+    def treinamento(self):  # funcao que ira realizar o treinamento
         varW = self.inicializarPesos(len(self.x[0]))
         varW0 = self.inicializarLimiar()
         varU = 0
+        y = 0  # variável que irá receber o sinal(u)
 
-        epoca = 0
-
-        y = 0 #variável que irá receber o sinal(u)
-
-        for i in range(len(self.x)):
+        erro = True
+        while erro:
 
             erro = False
 
-            while (erro != True):
-
-                # self.u.append(self.calculoSaida(self.x[i], varW, varW0))
+            for i in range(len(self.x)):
                 varU = self.calculoSaida(self.x[i], varW, varW0)
 
                 y = 1 if varU >= 0 else -1
 
-                if(y != self.d[i]):
+                if (y != self.d[i]):
                     varW = self.atualizarPesos(varW, self.n, (self.d[i] - y), self.x[i])
                     varW0 = self.atualizarLimiar(varW0, self.n, (self.d[i] - y))
-
+                    self.w.append(varW)
+                    self.w0.append(varW0)
+                    erro = True
                 else:
                     self.u.append(varU)
                     self.w.append(varW)
                     self.w0.append(varW0)
-                    erro = True
+            self.epoca += 1
+    # ----------------------------------------------------------------------
 
-                epoca += 1
-    #----------------------------------------------------------------------
-
-    #cálculo da saída (u)
+    # cálculo da saída (u)
     def calculoSaida(self, x, w, w0):
 
         resultado = 0
 
         for i in range(len(x)):
-            resultado += x[i]*w[i]
+            resultado += x[i] * w[i]
         resultado -= w0
 
         return resultado
-    #------------------------------------------------------------
+    # ------------------------------------------------------------
 
-    #método padrão para cálculo dos novos pesos
-    def atualizarPesos(self, w, n, erro, x): #funcao que irá atualizar os pesos
+    # método padrão para cálculo dos novos pesos
+    def atualizarPesos(self, w, n, erro, x):  # funcao que irá atualizar os pesos
         resultado = []
 
         for i in range(len(w)):
             resultado.append(
-                w[i] + n*erro*x[i]
+                w[i] + n * erro * x[i]
             )
 
         return resultado
 
-    def atualizarLimiar(self, w0, n, erro): #funcao que irá atualizar o limiar
-        return w0 + n*erro*(-1)
-    #-------------------------------------------------------------------------
+    def atualizarLimiar(self, w0, n, erro):  # funcao que irá atualizar o limiar
+        return w0 + n * erro * (-1)
+    # -------------------------------------------------------------------------
 
-    #inicializar pesos e limiar com dados aleatórios
+    # inicializar pesos e limiar com dados aleatórios
     def inicializarPesos(self, n):
         w = []
 
@@ -82,8 +82,30 @@ class Perceptron:
 
     def inicializarLimiar(self):
         return random.random()
-    #-----------------------------------------------
+    # -----------------------------------------------
 
     def resultado(self):
-        print(self.w)
+        return self.w[len(self.w) - 1]
 
+    def getW(self):
+        w = [self.w0[len(self.w0) - 1],
+             self.w[len(self.w) - 1][0],
+             self.w[len(self.w) - 1][1]]
+        return w
+
+    def predict(self, entradas, valoresDesejado, w1, w2):
+        normalizar = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(entradas)
+        entradas = normalizar.transform(entradas)
+        w0 = self.w0[len(self.w0)-1]
+
+        for i in range(len(entradas)):
+            print('x1: {0}  x2: {1} D: {2}'.format(entradas[i][0], entradas[i][1], valoresDesejado[i]))
+            resultado = entradas[i][0]*w1 + entradas[i][1]*w2 - w0
+            print('Resultado antes da operação: {0}'.format(resultado))
+            if resultado >= 0:
+                resultado = 1
+            elif resultado < 0:
+                resultado = -1
+
+            print('Iteração: {0} Resultado esperado: {1}  Resultado obtido: {2}'.format(i+1, valoresDesejado[i], resultado))
+            print('-------------------------------------------------------------------------------------------------------')
